b451632b46cf9bedde9c3d08f40aca85d7a42a15
samuel lutz <samlutz89@gmail.com>
2022-06-05 11:58:07 -0700

    dynamic adding doodler

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector('.grid')
+    const doodler = document.createElement('div')
+
+
+    // creates doodle
+    function createDoodler() {
+        grid.appendChild(doodler)
+        doodler.classList.add('doodler')
+    }
+    createDoodler()
+})
